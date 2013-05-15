0c24dfef24b0453c7b03eeb59e53f35797d79026
diellordielli <dd@feinheit.ch>
2013-05-15 08:09:35 +0200

    views for templates

---
+++ ruediwidmerch/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+
+from .cartoons.models import Cartoon
+from .cartoons.models import Category
+from .column.models import Column
+from .events.models import Event
+from .news.models import News
+from .portrait.models import Portrait
+
+
+def home(request):
+    cartoons = Cartoon.objects.all()
+
+    return render(request, 'index.html', {
+        'cartoons': cartoons,
+    })
+
+
+def column(request):
+    columns = Column.objects.all()
+
+    return render(request, 'texte.html', {
+        'columns': columns,
+    })
+
+
+def news(request):
+    newss = News.objects.all()
+    events = Event.objects.all()
+
+    return render(request, 'news.html', {
+        'newss': newss,
+        'events': events,
+    })
+
+
+def portrait(request):
+    portraits = Portrait.objects.all()
+
+    return render(request, 'portrait.html', {
+        'portraits': portraits,
+    })
+
+
+def contact(request):
+    return render(request, 'contact.html', {
+    })
