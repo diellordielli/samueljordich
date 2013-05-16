df02f390d19dfd695b1b6529e8c141671e63a92d
diellordielli <dd@feinheit.ch>
2013-05-16 14:31:25 +0200

    isotope correction

--- ruediwidmerch/static/js/isotope.js
+++ ruediwidmerch/static/js/isotope.js
@@ -1,31 +1,23 @@
 $(function(){
-  var $container = $('#container');
   
-  $container.isotope({
-    itemSelector: '.item',
-    masonry: {
-      columnWidth: 60
-    }
-  })
+    var $container = $('#container');
   
-  var $container = $('#container');
-  
-  $container.isotope({
-    itemSelector: '.item',
-    masonry: {
-      columnWidth: 60
-    }
-  })
-  
-  $('.item').click(function(){
-    var $this = $(this),
-        tileStyle = $this.hasClass('big') ? { width: 50, height: 50} : { width: 170, height: 110};
-    $this.toggleClass('big');
-    
-    $this.find('.portraitdesc').toggle();
-    $this.find('.portraitsubtitle').toggle();
-    $this.find('.portraittitle').toggleClass('portraittitle2');
-    $container.isotope( 'reLayout' )
-    
-  });
+    $container.isotope({
+        itemSelector: '.item',
+        masonry: {
+            columnWidth: 60
+        }
+    })
+
+    $('.item').click(function(){
+        var $this = $(this),
+            tileStyle = $this.hasClass('big') ? { width: 50, height: 50} : { width: 170, height: 110};
+        $this.toggleClass('big');
+
+        $this.find('.portraitdesc').toggle();
+        $this.find('.portraitsubtitle').toggle();
+        $this.find('.portraittitle').toggleClass('portraittitle2');
+        $container.isotope( 'reLayout' )
+
+    });
 });
