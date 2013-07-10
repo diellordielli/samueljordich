c5274dd1981db79c26222aefe3c42ae3711e5801
diellordielli <dd@feinheit.ch>
2013-07-10 15:52:09 +0200

    item category mouseover link

--- ruediwidmerch/static/js/applications.js
+++ ruediwidmerch/static/js/applications.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 
     $('.supernews img').css({'height': '100px'});
 
-    
+    $('.item.category').mouseenter(function() {
+        var $this = $(this);
+
+        $this.find('span').css({'color': '#000'});
+    });
+
+    $('.item.category').mouseleave(function() {
+        var $this = $(this);
+
+        $this.find('span').css({'color': '#FFF'});
+    });
 
 });
