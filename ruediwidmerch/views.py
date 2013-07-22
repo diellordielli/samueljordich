e75ded46e676d3e79ced62fd19b6d2f4fde3e05f
diellordielli <dd@feinheit.ch>
2013-07-22 15:56:30 +0200

    change view template

--- ruediwidmerch/views.py
+++ ruediwidmerch/views.py
@@ -43,7 +43,7 @@ def grafik(request):
 
     print cartoons
 
-    return render(request, 'index.html', {
+    return render(request, 'grafik.html', {
         'cartoons': cartoons,
         'categories': categories,
     })
